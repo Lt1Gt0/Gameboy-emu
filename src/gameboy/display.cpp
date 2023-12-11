a3d0e23ca83e957d54865c1c1dbad85e37d9825e
Lt1Gt0 <simonkaminski1100@gmail.com>
2023-12-10 22:42:25 -0700

    Added setup for SDL display

---
+++ src/gameboy/display.cpp
@@ -0,0 +1,15 @@
+#include "gameboy/display.hpp"
+#include <SDL2/SDL.h>
+
+namespace GameBoy
+{
+    Display::Display()
+    {
+
+    }
+
+    Display::~Display()
+    {
+
+    }
+}
---
+++ src/headers/gameboy/display.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef GB_DISPLAY_HPP
+#define GB_DISPLAY_HPP
+
+namespace GameBoy
+{
+    class Display
+    {
+        Display();
+        ~Display();
+    };
+}
+
+#endif // GB_DISPLAY_HPP
