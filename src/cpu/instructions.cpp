84cfb7dcd51fe7ac47b292c6ee1417062179b027
Lt1Gt0 <simonkaminski1100@gmail.com>
2023-12-17 19:37:23 -0700

    Added stubs for 8bit loads

--- src/cpu/instructions.cpp
+++ src/cpu/instructions.cpp
@@ -1,9 +1,302 @@
 #include "cpu/instructions.hpp"
 
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
 namespace GameBoy
 {
     namespace CPU
     {
+        int byte_ld(CPU* state, UNUSED word basePC, byte opcode)
+        {
+            switch (opcode) {
+                case 0x06:
+                {
+                    break;
+                }
+                case 0x0E:
+                {
+                    break;
+                }
+                case 0x16:
+                {
+                    break;
+                }
+                case 0x1E:
+                {
+                    break;
+                }
+                case 0x26:
+                {
+                    break;
+                }
+                case 0x2E:
+                {
+                    break;
+                }
+                case 0x7F:
+                {
+                    break;
+                }
+                case 0x78:
+                {
+                    break;
+                }
+                case 0x79:
+                {
+                    break;
+                }
+                case 0x7A:
+                {
+                    break;
+                }
+                case 0x7B:
+                {
+                    break;
+                }
+                case 0x7C:
+                {
+                    break;
+                }
+                case 0x7D:
+                {
+                    break;
+                }
+                case 0x7E:
+                {
+                    break;
+                }
+                case 0x40:
+                {
+                    break;
+                }
+                case 0x41:
+                {
+                    break;
+                }
+                case 0x42:
+                {
+                    break;
+                }
+                case 0x43:
+                {
+                    break;
+                }
+                case 0x44:
+                {
+                    break;
+                }
+                case 0x45:
+                {
+                    break;
+                }
+                case 0x46:
+                {
+                    break;
+                }
+                case 0x47:
+                {
+                    break;
+                }
+                case 0x48:
+                {
+                    break;
+                }
+                case 0x49:
+                {
+                    break;
+                }
+                case 0x4A:
+                {
+                    break;
+                }
+                case 0x4B:
+                {
+                    break;
+                }
+                case 0x4C:
+                {
+                    break;
+                }
+                case 0x4D:
+                {
+                    break;
+                }
+                case 0x4E:
+                {
+                    break;
+                }
+                case 0x50:
+                {
+                    break;
+                }
+                case 0x51:
+                {
+                    break;
+                }
+                case 0x52:
+                {
+                    break;
+                }
+                case 0x53:
+                {
+                    break;
+                }
+                case 0x54:
+                {
+                    break;
+                }
+                case 0x55:
+                {
+                    break;
+                }
+                case 0x56:
+                {
+                    break;
+                }
+                case 0x57:
+                {
+                    break;
+                }
+                case 0x58:
+                {
+                    break;
+                }
+                case 0x59:
+                {
+                    break;
+                }
+                case 0x5A:
+                {
+                    break;
+                }
+                case 0x5B:
+                {
+                    break;
+                }
+                case 0x5C:
+                {
+                    break;
+                }
+                case 0x5D:
+                {
+                    break;
+                }
+                case 0x5E:
+                {
+                    break;
+                }
+                case 0x5F:
+                {
+                    break;
+                }
+                case 0x60:
+                {
+                    break;
+                }
+                case 0x61:
+                {
+                    break;
+                }
+                case 0x62:
+                {
+                    break;
+                }
+                case 0x63:
+                {
+                    break;
+                }
+                case 0x64:
+                {
+                    break;
+                }
+                case 0x65:
+                {
+                    break;
+                }
+                case 0x66:
+                {
+                    break;
+                }
+                case 0x67:
+                {
+                    break;
+                }
+                case 0x68:
+                {
+                    break;
+                }
+                case 0x69:
+                {
+                    break;
+                }
+                case 0x6A:
+                {
+                    break;
+                }
+                case 0x6B:
+                {
+                    break;
+                }
+                case 0x6C:
+                {
+                    break;
+                }
+                case 0x6D:
+                {
+                    break;
+                }
+                case 0x6E:
+                {
+                    break;
+                }
+                case 0x70:
+                {
+                    break;
+                }
+                case 0x71:
+                {
+                    break;
+                }
+                case 0x72:
+                {
+                    break;
+                }
+                case 0x73:
+                {
+                    break;
+                }
+                case 0x74:
+                {
+                    break;
+                }
+                case 0x75:
+                {
+                    break;
+                }
+                case 0x36:
+                {
+                    break;
+                }
+            }
 
+            return 0;
+        }
+
+        int word_ld(CPU* state, UNUSED word basePC, byte opcode)
+        {
+            return 0;
+        }
+
+        int byte_alu(CPU* state, UNUSED word basePC, byte opcode)
+        {
+            return 0;
+        }
+
+        int word_alu(CPU* state, UNUSED word basePC, byte opcode)
+        {
+            return 0;
+        }
     }
 }
+
+#pragma GCC diagnostic error "-Wunused-parameter"
--- src/headers/cpu/instructions.hpp
+++ src/headers/cpu/instructions.hpp
@@ -3,15 +3,28 @@
 #define _GAMEBOY_CPU_INSTRUCTIONS_HPP_
 
 #include "utils/types.hpp"
+#include "cpu/cpu.hpp"
+
+#define UNUSED __attribute__((unused))
 
 namespace GameBoy
 {
     namespace CPU
     {
-        struct Instruction {
-            word opcode;
-            byte cycles;
-        };
+        // typedef int (*OP)(CPU* state, UNUSED word basePC, byte opcode);
+        // struct Instruction {
+        //     OP   targetFunc;
+        //     byte cycles;
+        // };
+        //
+        int byte_ld(CPU* state, UNUSED word basePC, byte opcode);
+        int word_ld(CPU* state, UNUSED word basePC, byte opcode);
+        int byte_alu(CPU* state, UNUSED word basePC, byte opcode);
+        int word_alu(CPU* state, UNUSED word basePC, byte opcode);
+
+        // inline Instruction instructionLookup[0x200] = {
+        //     [0x06] = {byte_ld, 8}
+        // };
     }
 }
 
